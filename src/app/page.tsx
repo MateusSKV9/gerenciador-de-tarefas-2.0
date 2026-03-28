71bad2a73da7791e30f15be15cfe82949a9dd0a4
Mateus Santos <mateussilvanzg@gmail.com>
2026-03-28 12:29:20 -0300

    feat: add badges and update theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { Plus } from "lucide-react";
+import { Plus, List, Check, CircleDot } from "lucide-react";
 
 export default function Home() {
 	return (
@@ -10,20 +11,36 @@ export default function Home() {
 			className="w-full h-screen bg-gray-100 flex flex-col
     justify-center items-center"
 		>
-			<h1 className="text-3xl font-bold">Gerenciador de Tarefas 2.0</h1>
-			<Card className="w-2xl p-4">
-				<div className="flex p-2 gap-2">
-					<Input type="text" placeholder="Adicionar tarefa" />
-					<Button
-						className="bg-blue-600 text-white cursor-pointer hover:bg-blue-700 hover:text-white"
-						variant={"outline"}
-					>
+			<h1 className="text-3xl font-bold m-2">Gerenciador de Tarefas 2.0</h1>
+			<Card className="w-2xl">
+				<CardHeader className="flex gap-2">
+					<Input className="p-4" type="text" placeholder="Adicionar tarefa" />
+					<Button className="cursor-pointer" variant={"default"}>
 						<Plus />
 						Cadastrar
 					</Button>
-				</div>
+				</CardHeader>
 
-				<Separator />
+				<CardContent>
+					<Separator className="mb-2" />
+
+					<div className="flex gap-2">
+						<Badge className="text-sm p-3 pt-3.5 pb-3.5 cursor-pointer">
+							<List size={18} />
+							Todas
+						</Badge>
+
+						<Badge variant={"secondary"} className="text-sm p-3 pt-3.5 pb-3.5cursor-pointer">
+							<CircleDot size={18} />
+							Não finalizadas
+						</Badge>
+
+						<Badge variant={"secondary"} className="text-sm p-3 pt-3.5 pb-3.5 cursor-pointer">
+							<Check size={18} />
+							Concluídas
+						</Badge>
+					</div>
+				</CardContent>
 			</Card>
 		</main>
 	);
